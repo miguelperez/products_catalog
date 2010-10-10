15c5dd9b71af925eb08d003185ff74e3ace27329
Miguel Perez <miguel.perez.leal@gmail.com>
2010-10-10 08:24:56 -0500

    Adding styles to the links in the table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  #returns a normal link that will be styled as a button
   def link(title, path, span_class = '')
     "<a href='#{path}' class='link'><span class='#{span_class}'>#{title}</span></a>"
   end
