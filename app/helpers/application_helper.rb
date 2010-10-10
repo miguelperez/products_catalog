fc52b7024f634194d2e536535bd957d18b8ff9d4
Miguel Perez <miguel.perez.leal@gmail.com>
2010-10-10 07:59:23 -0500

    Adding styles to links, buttons, inputs in the admin template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link(title, path, span_class = '')
+    "<a href='#{path}' class='link'><span class='#{span_class}'>#{title}</span></a>"
+  end
 end
