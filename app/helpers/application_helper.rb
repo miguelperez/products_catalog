cda48dfd6dc0839871e3313407c8e67d83741cb4
Miguel Perez <miguel.perez.leal@gmail.com>
2010-10-24 13:27:49 -0500

    adding the function that will allow the truncation of text depending on its value

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
   def link(title, path, span_class = '')
     "<a href='#{path}' class='link'><span class='#{span_class}'>#{title}</span></a>"
   end
+  
+  #truncates the received attribute if it can be truncated. Otherwise it will return the received object
+  def truncate_result(attribute, length = 20)
+    attribute = "#{attribute}"
+    begin
+      truncate(attribute, :length => length)
+    rescue Exception => e
+      attribute
+    end
+  end
 end
