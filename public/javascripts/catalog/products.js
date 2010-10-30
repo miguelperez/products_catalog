fde6d2ce50bec9b434b48f83bdae35dd0c406e84
Miguel Perez <miguel.perez.leal@gmail.com>
2010-10-30 13:15:52 -0500

    Fixing admin products view. Adding image widget

--- public/javascripts/catalog/products.js
+++ public/javascripts/catalog/products.js
@@ -1,11 +1,13 @@
 var Product = (function(){
-	var initialize = function(){
+	var imageContainerId = "#";
+	var initialize = function(containerId){
+		imageContainerId += containerId;
 		var thumbs = _('#thumbs').children();
 		var image = _(thumbs.first());
 		nextImage(image.attr('link'), image.attr('original'));
 		thumbs.each(function(index, image){
 			_(image).click(function(){
-				_('#selected_image').addClass('loading').html("");
+				_(imageContainerId).addClass('loading').html("");
 				nextImage(_(image).attr('link'),_(image).attr('original'));
 			});
 		});
@@ -15,23 +17,16 @@ var Product = (function(){
 		var img = new Image();
 		_(img).load(function () {
 	      _(this).hide();
-	    	_('#selected_image').removeClass('loading').append(this);
+	    	_(imageContainerId).removeClass('loading').append(this);
 	      // fade our image in to create a nice effect
-	      _(this).fadeIn();
-				applyZoom();
+	      _(this).show();
 	    }).error(function () {
 	      // notify the user that the image could not be loaded
 	    }).attr('src', link).attr('rel', original).attr('id', 'imageView');
 		return false;
 	};
-	
-	var applyZoom = function(){
-		_('#selected_image').imageView({width: 300, height:300});
-	};
-	
+		
 	return {
 		'initialize': initialize
 	};
 })();
-
-Product.initialize();
