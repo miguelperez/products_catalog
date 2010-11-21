ecd6e6d84b44a5b75ec4a98cc3e1e2ea50855b14
Miguel Perez <miguel.perez.leal@gmail.com>
2010-11-21 14:53:17 -0500

    fixing rendering of company info and contact form

--- config/initializers/catalog_configuration.rb
+++ config/initializers/catalog_configuration.rb
@@ -2,10 +2,17 @@
 require 'yaml'
 CATALOG_CONFS = YAML.load_file(File.join(RAILS_ROOT, 'config', 'catalog_configuration.yml'))
 
+def company_configuration_value(property)
+  value = CATALOG_CONFS['company'][property]
+  return h(value) unless value.blank?
+  ""
+end
+
 default_validations = {
   "product" => {
     "update" => {
       "not_visible_if_price_lower_than_zero" => "true",
+      "not_visible_if_no_images" => "true"
     }
   },
 }
